81aa81dd5dd5a42840c1445fc07bd96cd39a2f0f
holi-oli <oliviaszalai23@gmail.com>
2026-01-21 22:58:17 +0100

    Remove dead code (#9)

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -1,46 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using Unity.VisualScripting;
-using UnityEditor;
-using System;
 
 public class StartMenu : MonoBehaviour
 {
-    private Scene previousSceneName;
-    
-
-    private int sceneindex;
-    private int sceneToOpen;
-
-
-    private void Start()
-    {
-        sceneindex = SceneManager.GetActiveScene().buildIndex;
-        if (!PlayerPrefs.HasKey("previousScene" + sceneindex))
-        {
-            PlayerPrefs.SetInt("previousScene" + sceneindex, sceneindex);
-        }
-
-        sceneToOpen = PlayerPrefs.GetInt("previousScene" + sceneindex);
-
-
-
-    }
-
-    public void OnButtonClick()
-    {
-        SceneManager.LoadScene(sceneToOpen);
-    }
-
-
-    private void LoadSceneMode(Scene previousSceneName)
-    {
-        throw new NotImplementedException();
-    }
-
+  
     public void GameScene()
     {
         SceneManager.LoadScene("GameScene");
@@ -60,21 +24,6 @@ public class StartMenu : MonoBehaviour
     public void RecognizingBots()
     {
         SceneManager.LoadScene("RecognizingBots");
-    }
-
-    public void PauseScene()
-    {
-        SceneManager.LoadScene("PauseScene");
-    }
-
-    public void ExitGame()
-    {
-        Debug.Log("Spiel wird beendet...");
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
-    Application.Quit();
-#endif
     }
 
 
